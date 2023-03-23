f32b80a6b0b7afe78ef14e90bc381d54b25291c5
somin <angiwon12345@nate.com>
2023-03-23 19:05:26 +0900

    인증,인가 예외처리 SessionManagementFilter ConcurrentSessionFilter 익명사용자 인증 처리 동시 세션 제어 세션 고정 보호

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -3,16 +3,26 @@ package io.security.basicsecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -24,19 +34,58 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+    /*@Autowired
+    UserDetailsService userDetailsService;*/
+
+    @Bean
+    public UserDetailsManager users() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111") //패스워드 암호화 할때 어떤 알고리즘을 썻는지 적어줌
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "SYS", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager( user, sys, admin );
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .mvcMatchers("/login").permitAll()
+                .mvcMatchers("/user").hasRole("USER")
+                .mvcMatchers("/admin/pay").hasRole("ADMIN")
+                .mvcMatchers("/admin/**").access("hasRole('ADMIN')or hasRole('SYS')")
+                .anyRequest().authenticated(); //모든 설정에 인증을 받아야한다
 
         http
-                .formLogin();
-                //.loginPage("/loginPage")
-/*                .defaultSuccessUrl("/")
+                .formLogin()
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        RequestCache requestCache = new HttpSessionRequestCache();
+                        SavedRequest savedRequest = requestCache.getRequest(request, response);
+                        String redirectUrl = savedRequest.getRedirectUrl();
+                        response.sendRedirect(redirectUrl);
+
+                    }
+                });
+/*
+                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
                 .failureUrl("/login")
                 .usernameParameter("userId")
                 .passwordParameter("passwd")
@@ -55,7 +104,8 @@ public class SecurityConfig {
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll();*/
+                .permitAll();
+
 
         http
                 .logout()
@@ -81,6 +131,30 @@ public class SecurityConfig {
                 .rememberMeParameter("remember")//remember-me 디폴트값
                 .tokenValiditySeconds(3600)
                 .userDetailsService(userDetailsService); //유저 계정을 조회할때
+
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);
+
+
+*/
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.sendRedirect("/denied"); //인증을 받은 사용자가 인가 실패 했기 때문에 따로 인증권한 설정을 안해도됨
+                    }
+                });
+
+
 
 
 
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -12,8 +12,26 @@ public class SecurityController {
         return "home";
     }
 
-    @GetMapping("loginPage")
-    public String loginPage() {
+    @GetMapping("/user")
+    public String user() {
         return "loginPage";
+    }
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "loginPage";
+    }
+    @GetMapping("/admin/**")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "denied";
     }
 }
