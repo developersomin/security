b7129165691b31a19972228f434adbd07e498a93
somin <angiwon12345@nate.com>
2023-03-27 23:38:19 +0900

    실전 프로젝트

---
+++ src/main/java/io/security/basicsecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package io.security.basicsecurity.service.impl;
+
+import io.security.basicsecurity.domain.Account;
+import io.security.basicsecurity.repository.UserRepository;
+import io.security.basicsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void createUser(Account account) {
+        userRepository.save( account );
+    }
+}
